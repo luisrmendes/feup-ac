b377a912e1c621f3a2cdf4f0073483935f12dee0
lrmendes98 <lrmmendes98@gmail.com>
2021-11-15 11:39:49 +0000

    display average loan duration

--- teste.py
+++ teste.py
@@ -1,13 +1,28 @@
 # load csv module
 import csv
+import pandas as pd
+
+def getAverage(num):
+    sum_num = 0
+    for t in num:
+        sum_num = sum_num + t           
+
+    avg = sum_num / len(num)
+    return avg
+
+file_name = 'ficheiros_competicao/loan_test.csv'
 
 # open file for reading
-file_name = 'ficheiros_competicao/loan_test.csv'
-with open(file_name) as csvDataFile:
+# with open(file_name) as csvDataFile:
 
-    # read file as csv file, demilitador = ;
-    csvReader = csv.reader(csvDataFile, delimiter=';')
+#     # read file as csv file, demilitador = ;
+#     csvReader = csv.reader(csvDataFile, delimiter=';')
 
-    # for every row, print the row
-    for row in csvReader:
-        print(row)
+#     # for every row, print the row
+#     for row in csvReader:
+#         print(row)
+
+df = pd.read_csv(file_name, delimiter=';')
+# print(df.loc[:,"duration"].to_list())
+print(getAverage(df.loc[:,"duration"].to_list()))
+# print(df.loc[0])
