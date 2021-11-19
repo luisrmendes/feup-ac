80a8e236c6435e27cf0ae244b2dcbcff25f9048a
lrmendes98 <lrmmendes98@gmail.com>
2021-11-19 16:17:42 +0000

    add plot, getAverage

---
+++ python_src/data/utils.py
@@ -0,0 +1,7 @@
+def getAverage(list):
+    sum_num = 0
+    for t in list:
+        sum_num += t           
+
+    avg = sum_num / len(list)
+    return avg
--- python_src/main.py
+++ python_src/main.py
@@ -1,6 +1,8 @@
 from data.bank_data import Bank_Data
 from data.district import District
 from data.enum_types import Order
+from data.utils import *
+import matplotlib.pyplot as plt
 
 data = Bank_Data()
 
@@ -9,10 +11,27 @@ data.add_clients("data/ficheiros_competicao/client.csv")
 data.add_accounts("data/ficheiros_competicao/account.csv")
 data.add_dispositions("data/ficheiros_competicao/disp.csv")
 
-list = data.districts
-list = data.quicksort(list, District.get_n_inhab, Order.Decreasing)
-list = data.filter(list, District.get_average_salary, "<", 8200)
-for member in list:
-    member.print()
+# list = data.districts
+# list = data.quicksort(list, District.get_n_inhab, Order.Decreasing)
+# list = data.filter(list, District.get_average_salary, "<", 8200)
+# for member in list:
+#     member.print()
 
-data.print()
+# data.print()
+
+
+list2 = data.districts
+all_avg_salary = data.get_all(list2, District.get_average_salary)
+
+print(getAverage(all_avg_salary))
+
+
+# left_coordinates = [1, 2, 3, 4, 5]
+# heights = [10, 20, 30, 15, 40]
+# bar_labels = ['One', 'Two', 'Three', 'Four', 'Five']
+# plt.bar(left_coordinates, heights, tick_label=bar_labels,
+#         width=0.6, color=['red', 'black'])
+# plt.xlabel('X-axis')
+# plt.ylabel('Y-axis')
+# plt.title("A simple bar graph")
+# plt.show()
