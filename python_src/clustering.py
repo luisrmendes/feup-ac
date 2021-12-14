0731ed2002dcd4e33f446b55a669b9101fb714af
FilipeAlmeidaFEUP <filipe.almeida.flp18@gmail.com>
2021-12-14 01:52:14 +0000

    Lembro-me de um menino que andava sozinho

--- python_src/clustering.py
+++ python_src/clustering.py
@@ -1,10 +1,9 @@
-import re
-from pandas import DataFrame
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, SpectralClustering, Birch
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
+#KMeans----------------------------------------------------
 
 Data = pd.read_csv(
         'csv_files/dataTrain.csv',
@@ -19,19 +18,46 @@ for i in range(44):
     scaler.fit(Data[[i]])
     Data[[i]] = scaler.transform(Data[[i]])
 
-k_rng = range(1, 20)
+k_rng = range(1, 10)
 sse = []
 for k in k_rng:
-    km = KMeans(n_clusters = k).fit(Data)
+    km = KMeans(n_clusters = k)
+    km.fit(Data)
     sse.append(km.inertia_)
 
 plt.plot(k_rng, sse)
+plt.show()
+
   
-results = KMeans(n_clusters=8).fit_predict(Data)
-kmeans = KMeans(n_clusters=8).fit(Data)
+results = KMeans(n_clusters=2).fit_predict(Data)
+kmeans = KMeans(n_clusters=2).fit(Data)
 centroids = kmeans.cluster_centers_
 print(results)
 
-plt.scatter(Data[0], Data[2], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
-plt.scatter(centroids[:, 0], centroids[:, 2], c='red', s=50)
-plt.show()
+#plt.scatter(Data[0], Data[2], c= kmeans.labels_.astype(float), s=50, alpha=0.5)
+#plt.scatter(centroids[:, 0], centroids[:, 2], c='red', s=50)
+#plt.show()
+
+
+#SpectralClustering----------------------------------------------------
+results1 = SpectralClustering(n_clusters=2).fit_predict(Data)
+spectralClustering = SpectralClustering(n_clusters=2).fit(Data)
+print(results1)
+
+
+#Birch----------------------------------------------------
+results2 = Birch(n_clusters=None).fit_predict(Data)
+birch = Birch(n_clusters=None).fit(Data)
+print(results2)
+
+data = pd.read_csv(
+        'csv_files/dataTrain.csv',
+        sep= ',', header = None)
+
+data[45] = results
+data[46] = results1
+data[47] = results2
+
+print (data)
+
+data.to_csv('clustering.csv', index = False)
